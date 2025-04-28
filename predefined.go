aef14e81afaf98b77bbf5a36852de5f8378bbaf9
Nikita Kanashin <nikita@kanash.in>
2025-04-28 10:18:36 +0300

    feat: add predefined parsers for int64

--- predefined.go
+++ predefined.go
@@ -22,6 +22,14 @@ func parseInt(ctx context.Context, v string) (int, error) {
 	return i, nil
 }
 
+func parseInt64(ctx context.Context, v string) (int64, error) {
+	i, err := strconv.ParseInt(v, 10, 64)
+	if err != nil {
+		return 0, fmt.Errorf("invalid int64 value: %s", v)
+	}
+	return i, nil
+}
+
 var QueryParamBool = func(name string) *QueryParamType[bool] {
 	return QueryParam(name, parseBool)
 }
@@ -36,4 +44,12 @@ var QueryParamInt = func(name string) *QueryParamType[int] {
 
 var QueryParamIntMaybe = func(name string) *QueryParamMaybeType[int] {
 	return QueryParamMaybe(name, parseInt)
+}
+
+var QueryParamInt64 = func(name string) *QueryParamType[int64] {
+	return QueryParam(name, parseInt64)
+}
+
+var RouterParamInt64 = func(name string) *RouterParamType[int64] {
+	return RouterParam(name, parseInt64)
 }
